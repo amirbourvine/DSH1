9193833abef2470c5803d7431101d1d8f61aa8df
amirb <amirbourvine@campus.technion.ac.il>
2022-12-05 23:15:15 +0200

    during tests

--- worldcup23a1.cpp
+++ worldcup23a1.cpp
@@ -509,15 +509,17 @@ StatusType world_cup_t::get_all_players(int teamId, int* const output){
 bool isInRange(const shared_ptr<Team>& team, int minTeamId, int maxTeamId){
     return team->getTeamId() >= minTeamId && team->getTeamId() <= maxTeamId;
 }
-output_t<int> world_cup_t::knockout_winner(int minTeamId, int maxTeamId){
-    if(minTeamId < 0 || maxTeamId < 0 || maxTeamId < minTeamId)
+output_t<int> world_cup_t::knockout_winner(int minTeamId, int maxTeamId) {
+    if (minTeamId < 0 || maxTeamId < 0 || maxTeamId < minTeamId)
         return StatusType::INVALID_INPUT;
 
 
     this->printValidTeams();
     shared_ptr<Team> playingTeam = findMinValid(minTeamId, maxTeamId);
-    if(playingTeam == nullptr)
+    if (playingTeam == nullptr){
+        cout<<"GREAT"<<endl;
         return StatusType::FAILURE;
+}
 
     int size = 0;
     shared_ptr<Team> temp = playingTeam;
